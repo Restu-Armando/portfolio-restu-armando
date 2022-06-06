a1b8a10e324e849428c2d81ce7765e9929285c24
YEES <peopleunknwon09@gmail.com>
2022-06-06 21:11:08 +0700

    portfolio tailwind restu

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+// humberger 
+const humberger = document.querySelector('#humberger');
+const navmenu = document.querySelector('#nav-menu');
+
+humberger.addEventListener('click', function() {
+  humberger.classList.toggle('humberger-active');
+  navmenu.classList.toggle('hidden');
+});
+
+//navbar-fixed
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if(window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  }else {
+    header.classList.remove('navbar-fixed')
+  }
+}
