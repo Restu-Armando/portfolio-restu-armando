697cde499b5d2846480d77a520a8a0112e3ec61f
YEES <peopleunknwon09@gmail.com>
2022-06-07 16:46:44 +0700

    back to top and smooth scrolling

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,10 +11,23 @@ humberger.addEventListener('click', function() {
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
   if(window.pageYOffset > fixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   }else {
-    header.classList.remove('navbar-fixed')
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
 }
+
+// klick out of navbar to close navbar (phone version)
+window.addEventListener('click',function (e) {
+  if (e.target !=navmenu && e.target !=humberger) {
+    humberger.classList.remove('humberger-active');
+    navmenu.classList.toggle('hidden');
+  }
+});
