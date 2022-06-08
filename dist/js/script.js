92d7e3488f12d2f1c38d88401cb0188155b55045
YEES <peopleunknwon09@gmail.com>
2022-06-08 21:59:35 +0700

    update dark mode, hide humberger button and other feature

--- dist/js/script.js
+++ dist/js/script.js
@@ -26,8 +26,29 @@ window.onscroll = function () {
 
 // klick out of navbar to close navbar (phone version)
 window.addEventListener('click',function (e) {
-  if (e.target !=navmenu && e.target !=humberger) {
+  if (e.target !=humberger && e.target !=navmenu ) {
     humberger.classList.remove('humberger-active');
-    navmenu.classList.toggle('hidden');
+    navmenu.classList.add('hidden');
   }
 });
+
+// dark mode toggole
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+  if (darkToggle.checked){
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  }  else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+//position toggle as mode / local storage
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+   darkToggle.checked = true;
+} else {
+   darkToggle.checked = false;
+}
